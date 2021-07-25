c8f020f8024c30a8ad6ce7b541f4a2508a6fa36a
carolinecole25 <carolinecole25@gmail.com>
2021-07-25 11:14:40 +0000

    manage categories functionality added

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge:"right"});
     $('.collapsible').collapsible();
+    $("select").formSelect();
   });
 
   // Credit: code taken from Task ManagerAuth, linked in README.md
