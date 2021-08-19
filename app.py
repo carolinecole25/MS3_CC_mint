402563c7f49b14ebed7a2d6574e07eab2ac4fc28
carolinecole25 <carolinecole25@gmail.com>
2021-08-19 16:06:10 +0000

    app.py changed to false to deploy site

--- app.py
+++ app.py
@@ -307,4 +307,4 @@ def error_404(error: object) -> object:
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=false)
+            debug=true)
